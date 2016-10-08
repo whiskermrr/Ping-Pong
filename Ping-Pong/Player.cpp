f4194988ae8d04a0466a5ada569126e712431437
whiskermrr <witek119@gmail.com>
2016-10-08 17:33:37 +0300

    Fixed velocity problem and corrupted file
    -Fixed velocity
    -Added functionality to canMoveUp and canMoveDown
    -Had problem with one of file after break in power supply

--- Ping-Pong/Player.cpp
+++ Ping-Pong/Player.cpp
@@ -28,26 +28,40 @@ void Player::Update()
 	switch (this->objectNumber)
 	{
 	case 1:
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && canMoveUp)
-			this->velocity.y -= 10.f;
-
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && canMoveDown)
-			this->velocity.x += 10.f;
-
+		this->velocity.y = 10 * (sf::Keyboard::isKeyPressed(sf::Keyboard::S) - sf::Keyboard::isKeyPressed(sf::Keyboard::W));
 		break;
 
 	case 2:
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && canMoveUp)
-			this->move(0.f, -10.f);
-
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && canMoveDown)
-			this->move(0.f, 10.f);
-		
+		this->velocity.y = 10 * (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) - sf::Keyboard::isKeyPressed(sf::Keyboard::Up));
 		break;
 
 	default:
 		break;
 	}
 
-	Entity::Update();
+	if (this->getPosition().y <= 0)
+	{
+		this->canMoveUp = false;
+	}
+
+	else if (this->getPosition().y + this->getGlobalBounds().height >= 600)
+	{
+		this->canMoveDown = false;
+	}
+
+	else if (!this->canMoveDown || !this->canMoveUp)
+	{
+		canMoveDown = true;
+		canMoveUp = true;
+	}
+
+	if (this->velocity.y < 0 && this->canMoveUp)
+	{
+		Entity::Update();
+	}
+
+	else if (this->velocity.y > 0 && this->canMoveDown)
+	{
+		Entity::Update();
+	}
 }
